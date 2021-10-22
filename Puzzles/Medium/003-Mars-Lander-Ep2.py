1262e87deb24a27b7d61518fa4d31455201cd865
Guilherme Baos <guilhermebaos@gmail.com>
2021-10-22 12:17:00 +0100

    Mars Lander Ep.2 WIP
    Almost Done!

--- Puzzles/Medium/003-Mars-Lander-Ep2.py
+++ Puzzles/Medium/003-Mars-Lander-Ep2.py
@@ -51,47 +51,65 @@ while True:
     # CONTOL SYSTEM
     new_rotate = 0
     new_power = 3
+    min_h_speed = 10  # Minimum acceptable horizontal speed
 
-    # Start landing protocols
-    if dist == 0:
-        # Vertical Speed Control
-        if abs(v_speed) > 35:
-            new_power = 4
-        elif abs(v_speed) > 25:
-            new_power = 3
-        else:
-            new_power = 0
+    # DATA ANALASYS
+
+    # Horizontal Speed Info
+    delta_h_speed = abs(min_h_speed - abs(h_speed))  # Delta v we need to get to min_h_speed
+    h_acc = math.sin(math.radians(45)) * 4  # Horizontal Acceleration
+
+    time_to_h_decelerate = 1.2 * delta_h_speed / h_acc  # Time to decelerate given by Delta v over a
+
+    delta_h_pos = time_to_h_decelerate * (h_speed + min_h_speed) / 2  # Delta h position if we start breaking now
+
+    final_h_pos = x + delta_h_pos
 
     # Get to landing area ASAP
     if dist != 0:
         new_rotate = -45 * direction
         new_power = 4
 
-    # HSC -> Horizontal Speed Control
-    min_h_speed = 15  # Minimum acceptable horizontal speed
+        # HSC -> HORIZONTAL SPEED CONTROL
 
-    delta_h_speed = abs(min_h_speed - abs(h_speed))  # Delta v we need to get to min_h_speed
-    h_acc = math.sin(math.radians(45)) * 4  # Horizontal Acceleration
+        # Activate HSC if we are already going to stop in the right area if we start breaking now, Position = x + vt, where v is average speed of the interval!
+        if (flat[0] < final_h_pos < flat[1]):
+            if abs(h_speed) > min_h_speed:
+                new_rotate = 45 * (1 if h_speed > 0 else -1)
 
-    time_to_h_decelerate = delta_h_speed / h_acc  # Time to decelerate given by Delta v over a
+        elif (flat[0] < final_h_pos and dist >= 0) or (flat[1] > final_h_pos and dist <= 0):
+            new_rotate = 45 * (1 if h_speed > 0 else -1)
 
-    delta_h_pos = time_to_h_decelerate * (
-                h_speed + min_h_speed) / 2 * direction  # Delta h position if we start breaking now
+        # VSC -> VERTCAL SPEED CONTROL
 
-    final_h_pos = x + delta_h_pos
-    print(final_h_pos, dist, flat, file=sys.stderr)
+        # Make sure we keep a constant vertical speed
+        if abs(new_rotate) > max_angle:
+            if abs(v_speed) > 35:
+                new_rotate = -max_angle * direction
 
-    # Activate HSC if we are already going to stop in the right area if we start breaking now, Position = x + vt, where v is average speed of the interval!
-    if (flat[0] < final_h_pos and dist >= 0) or (flat[1] > final_h_pos and dist <= 0):
+    # LP -> LANDING PROTOCOL
+
+    # Start landing protocols
+    if dist == 0:
+        # Local HSC (Horizontal Speed Control)
+        if flat[0] + 100 > final_h_pos:
+            new_rotate = -45
+            new_power = 4
+        elif flat[1] - 100 < final_h_pos:
+            new_rotate = 45
+            new_power = 4
+
         if abs(h_speed) > min_h_speed:
-            new_rotate = 45 * 1 if h_speed > 0 else -1
+            new_rotate = 45 * (1 if h_speed > 0 else -1)
+            new_power = 4
 
-    # VSC -> Vertical Speed Control
-
-    # Make sure we keep a constant vertical speed
-    if abs(new_rotate) > max_angle:
-        if abs(v_speed) > 35:
-            new_rotate = -max_angle * direction
+        # Local VSC (Vertical Speed Control)
+        print(v_speed, file=sys.stderr)
+        if v_speed < -35:
+            new_power = 4
+            new_rotate = 0
+        elif v_speed > -15:
+            new_power = 3
 
     # print(rotate power)
     print(f'{int(new_rotate)} {new_power}')
